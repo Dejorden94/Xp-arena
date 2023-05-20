52604e6f3ee0b67470cbcc317ac46e860a8e65b4
Dejorden <d.moerman@hotmail.nl>
2023-05-20 17:45:55 +0200

    add CompletedTask model

---
+++ app/Models/CompletedTask.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompletedTask extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'task_id',
+        'user_id',
+        'game_id',
+        'completion_status',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+}
