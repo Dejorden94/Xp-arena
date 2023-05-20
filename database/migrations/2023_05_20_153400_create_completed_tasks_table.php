020da4ebc02203c678d409f717b7d2037b2a1750
Dejorden <d.moerman@hotmail.nl>
2023-05-20 17:35:23 +0200

    add needed collumns

--- database/migrations/2023_05_20_153400_create_completed_tasks_table.php
+++ database/migrations/2023_05_20_153400_create_completed_tasks_table.php
@@ -4,24 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCompletedTasksTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('completed_tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('game_id')->constrained()->onDelete('cascade');
+            $table->boolean('completion_status')->default(false);
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('completed_tasks');
     }
-};
+}
