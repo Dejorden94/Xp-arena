df5cd03c36d8bc61fb48140a3a0b62958200a8b8
Dejorden <d.moerman@hotmail.nl>
2023-05-20 17:47:29 +0200

    add relation to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-    
-    public function game()
-{
-    return $this->belongsTo(Game::class);
-}
 
+    public function game()
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    public function completedTasks()
+    {
+        return $this->hasMany(CompletedTask::class);
+    }
 }
