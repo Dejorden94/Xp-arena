30786ce06257e2e3f65789395956c6cfcc6ba527
Dejorden <d.moerman@hotmail.nl>
2023-05-27 21:10:45 +0200

    Removed the TaskController from api route file

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GameController;
-use App\Http\Controllers\TaskController;
 
 
 
@@ -22,11 +21,6 @@ use App\Http\Controllers\TaskController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/tasks/{taskId}/complete', [TaskController::class, 'completeTask']);
-Route::get('/tasks/unverified', [TaskController::class, 'getUnverifiedTasks']);
-Route::put('/tasks/{taskId}/verify', [TaskController::class, 'verifyTask']);
-
 
 Route::get('/user/{id}', [UserController::class, 'getUserById']);
 
