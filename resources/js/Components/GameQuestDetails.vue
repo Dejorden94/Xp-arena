a32e144a5db906f851323a960ba5ff2ec293487b
Dejorden <d.moerman@hotmail.nl>
2023-10-24 17:04:40 +0200

    add field to add criteria

--- resources/js/Components/GameQuestDetails.vue
+++ resources/js/Components/GameQuestDetails.vue
@@ -1,6 +1,17 @@
 <template>
     <article>
-        <h1>Quest details van {{ quest.name }}</h1>
+        <h1>Quest details van {{ questName }}</h1>
+
+        <button @click="showAddCriteriaForm = true">Voeg criteria toe</button>
+
+        <section v-if="showAddCriteriaForm">
+            <input v-model="newCriterionName" placeholder="Naam van criterium">
+            <button @click="addCriteria">Opslaan</button>
+        </section>
+
+        <ul>
+            <li v-for="criterion in criteria" :key="criterion.id">{{ criterion.name }}</li>
+        </ul>
     </article>
 </template>
 <script>
@@ -11,8 +22,32 @@ export default {
             required: true
         }
     },
-    created() {
-        console.log("GameQuestDetails component is geïnitialiseerd!");
+    data() {
+        return {
+            criteria: [],
+            showAddCriteriaForm: false,
+            newCriterionName: ''
+        };
+    },
+    computed: {
+        questName() {
+            return this.quest && this.quest.name ? this.quest.name : 'Loading...';
+        }
+    },
+
+    methods: {
+        async addCriteria() {
+            try {
+                const response = await axios.post(`/api/tasks/${this.quest.id}/criteria`, {
+                    name: this.newCriterionName
+                });
+                this.criteria.push(response.data);  // Voeg het nieuwe criterium toe aan de lijst
+                this.newCriterionName = '';  // Reset het invoerveld
+                this.showAddCriteriaForm = false;  // Verberg het formulier
+            } catch (error) {
+                console.error("Er is een fout opgetreden bij het toevoegen van het criterium:", error);
+            }
+        }
     }
 }
 </script>
@@ -21,6 +56,8 @@ article {
     height: 5rem;
     width: 80vw;
     margin: 0 auto;
-    background: red;
+    background: var(--background-super-dark);
+    border: 2px solid var(--background-lighter);
+    border-radius: 1rem;
 }
 </style>
